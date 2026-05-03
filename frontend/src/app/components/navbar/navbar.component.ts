e156e0581144396e532dd63e218efe25a289f607
Rauliitoo4 <raulredondochicano@gmail.com>
2026-05-03 12:35:52 +0200

    Navbar creado

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isDarkMode = signal(false);
+  cartItemCount = signal(0);
 
+  toggleDarkMode() {
+    this.isDarkMode.update(v => !v);
+    document.body.classList.toggle('dark-theme', this.isDarkMode());
+  }
 }
