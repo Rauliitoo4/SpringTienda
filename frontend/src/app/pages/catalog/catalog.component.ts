b7fb8f9db4158ca3d42171f6afcf52878dbdbbd5
Rauliitoo4 <raulredondochicano@gmail.com>
2026-05-03 13:39:12 +0200

    página de catálogo terminada

--- frontend/src/app/pages/catalog/catalog.component.ts
+++ frontend/src/app/pages/catalog/catalog.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatSelectModule, MatFormFieldModule, CommonModule, FormsModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
+  selectedFilter = signal('todo');
+  selectedSort = 'relevantes';
 
+  setFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
 }
